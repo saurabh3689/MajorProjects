6ec9e91c018bc4e63dda7b1224c3196006c86c44
saurabh3689 <dsaurabh077@gmail.com>
2026-09-13 17:23:55 +0530

    Update socket connection URL to production

--- LinkedIn/frontend/src/context/userContext.jsx
+++ LinkedIn/frontend/src/context/userContext.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 export const userDataContext = createContext()
 import {io} from"socket.io-client"
 
-export let socket = io("http://localhost:8000")
+export let socket = io("https://majorprojects-0fps.onrender.com")
 
 function UserContext({children}) {
 let [userData, setUserData] = useState(null)
