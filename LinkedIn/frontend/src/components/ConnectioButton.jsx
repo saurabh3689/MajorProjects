04a179adb8dc2f539a8b7aa824d894cd4639bb04
saurabh3689 <dsaurabh077@gmail.com>
2026-09-13 17:25:00 +0530

    Update socket connection URL to production

--- LinkedIn/frontend/src/components/ConnectioButton.jsx
+++ LinkedIn/frontend/src/components/ConnectioButton.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 import io from "socket.io-client"
 import { userDataContext } from '../context/userContext'
 import { useNavigate } from 'react-router-dom'
-const socket = io("http://localhost:8000")
+const socket = io("https://majorprojects-0fps.onrender.com")
 function ConnectioButton({userId}) {
 let {serverUrl} = useContext(authDataContext)
 let {userData, setUserData} = useContext(userDataContext)
