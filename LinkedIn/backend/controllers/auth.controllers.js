f06c08a343624c814d7434e1993c7bda0b2a2496
saurabh3689 <dsaurabh077@gmail.com>
2026-09-16 11:36:45 +0530

    fix production cookie

--- LinkedIn/backend/controllers/auth.controllers.js
+++ LinkedIn/backend/controllers/auth.controllers.js
@@ -27,7 +27,7 @@ export const signUp = async (req, res) => {
         res.cookie("token", token,{
             httpOnly:true,
             maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
+            sameSite: "none",
             secure: process.env.NODE_ENV === "production" ? true : false
         })
 
