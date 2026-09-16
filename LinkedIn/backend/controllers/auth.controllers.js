5f2acb0c56ee63497aba30cf0079bab7baf6eb54
saurabh3689 <dsaurabh077@gmail.com>
2026-09-16 11:42:57 +0530

    cookie issu

--- LinkedIn/backend/controllers/auth.controllers.js
+++ LinkedIn/backend/controllers/auth.controllers.js
@@ -58,7 +58,7 @@ export const login = async (req, res) => {
         res.cookie("token", token,{
             httpOnly:true,
             maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
+            sameSite: "none",
             secure: process.env.NODE_ENV === "production" ? true : false
         })
         return res.status(201).json(user)
